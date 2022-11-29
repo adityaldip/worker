8d62110c5a3ab2b712ae8f90973931360f596724
adityaldi161 <adityaldi@gmail.com>
2022-11-29 10:31:37 +0700

    fixing jika salah satu mapping kosong

--- app/helpers/accurate.helper.js
+++ app/helpers/accurate.helper.js
@@ -376,10 +376,7 @@ class AccurateHelper {
 
             const body = accurateMapping.payout(order)
             const payload = this.payloadBuilder(endpoint, body)
-            console.log("body", body)
-            console.log("detail diskon",body.detailInvoice[0].detailDiscount)
             const response = await request.requestPost(payload)
-            console.log(response.d)
             await helper.accurateLog({
                 created_at: new Date(),
                 type: 'ORDER',
--- app/mappings/accurate/payout.mapping.js
+++ app/mappings/accurate/payout.mapping.js
@@ -23,31 +23,41 @@ const payoutMapping = (order) => {
 
     const mappinv = [];
     order.invoice_mapped.forEach((i, e) => {
+        let detailDiscount = [];
+        if(order.platform_rebate){
+            detailDiscount.push({
+                accountNo: order.platform_rebate.no, // required
+                amount: i.platform_rebate, // reqired
+            })
+        }
+        if(order.voucher_seller){
+            detailDiscount.push({
+                accountNo: order.voucher_seller.no, // required
+                amount: i.voucher_seller, // reqired
+            })
+        }
+        if(order.fulfillment){
+            detailDiscount.push({
+                accountNo: order.fulfillment.no, // required
+                amount: i.platform_fulfilment_fee, // reqired
+            })
+        }
+        if(order.service){
+            detailDiscount.push({
+                accountNo: order.service.no, // required
+                amount: i.service_fee, // reqired
+            })
+        }
+        if(order.shipping_difference){
+            detailDiscount.push({
+                accountNo: order.shipping_difference.no, // required
+                amount: i.cashless_shipping_difference, // reqired
+            })
+        }
         const mapinv = {
             invoiceNo: order.invoice.number, // required
             paymentAmount: total, // required
-            detailDiscount: [
-                {
-                    accountNo: order.platform_rebate.no, // required
-                    amount: i.platform_rebate, // reqired
-                },
-                {
-                    accountNo: order.shipping_difference.no, // required
-                    amount: i.cashless_shipping_difference, // reqired
-                },
-                {
-                    accountNo: order.voucher_seller.no, // required
-                    amount: i.voucher_seller, // reqired
-                },
-                {
-                    accountNo: order.fulfillment.no, // required
-                    amount: i.platform_fulfilment_fee, // reqired
-                },
-                {
-                    accountNo: order.service.no, // required
-                    amount: i.service_fee, // reqired
-                },
-            ]
+            detailDiscount: detailDiscount
         }
         return mappinv[e] = mapinv
     });
