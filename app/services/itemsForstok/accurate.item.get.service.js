89bd29d32f1f3f7f86bf9e1f6f2f6ac08217e76a
aditya aldi <aldi.prasetya@forstok.com>
2023-08-07 10:37:31 +0700

    remove checking item on items get

--- app/services/itemsForstok/accurate.item.get.service.js
+++ app/services/itemsForstok/accurate.item.get.service.js
@@ -22,76 +22,52 @@ const accurate = new AccurateHelper()
 const getItemForstok = async (id, channel, msg) => {
     try {
         const profileId = parseInt(id)
-        const item = await itemModel.find({
-            profile_id: profileId
-            // synced: true
-        })
 
         const seller = await sellerModel.findBy({
             seller_id: profileId,
         })
         accurate.setAccount(seller)
-
-        const loopitem = await item.toArray();
         const event = await eventModel.findBy({
             profile_id: profileId,
-            status: { $ne: 'completed' }
+            status: {
+                $ne: 'completed'
+            }
         });
-        if (loopitem.length > 0) {
-            //Update event item_forstok_count
-            const updateEvent = await eventModel.update({
-                profile_id: profileId,
-                status: "running"
-            }, {
-                $set: {
-                    item_forstok_count: loopitem.length
-                },
-            })
 
-            if (!updateEvent) {
-                throw Error('Event tidak terupdate!')
-            } else {
-                for(const wh of seller.warehouses) {
-                    if (wh.accurate_warehouse && wh.accurate_warehouse.name !== "") {
-                        const TotalPage = await accurate.getTotalDataStockByWarehouse(wh.accurate_warehouse.name)
-                        if (TotalPage.pageCount > 1) {
-                            for (let index = 1; index <= TotalPage.pageCount; index++) {
-                                setTimeout(async () => {
-                                    const dataDelayed = {
-                                        eventID: event._id.toString(),
-                                        warehouseName: wh.accurate_warehouse.name,
-                                        page: index
-                                    }
-                                    await delayed.insert({
-                                        profile_id: profileId,
-                                        queue:"accurate_items_fetch",
-                                        payload:dataDelayed,
-                                        in_progress:0,
-                                        priority: 2,
-                                        created_at:new Date()
-                                    })
-                                }, index * 300);
+        // //Update event item_forstok_count
+        // const updateEvent = await eventModel.update({
+        //     profile_id: profileId,
+        //     status: "running"
+        // }, {
+        //     $set: {
+        //         item_forstok_count: loopitem.length
+        //     },
+        // })
+        for (const wh of seller.warehouses) {
+            if (wh.accurate_warehouse && wh.accurate_warehouse.name !== "") {
+                const TotalPage = await accurate.getTotalDataStockByWarehouse(wh.accurate_warehouse.name)
+                if (TotalPage.pageCount > 1) {
+                    for (let index = 1; index <= TotalPage.pageCount; index++) {
+                        setTimeout(async () => {
+                            const dataDelayed = {
+                                eventID: event._id.toString(),
+                                warehouseName: wh.accurate_warehouse.name,
+                                page: index
                             }
-                        }
+                            await delayed.insert({
+                                profile_id: profileId,
+                                queue: "accurate_items_fetch",
+                                payload: dataDelayed,
+                                in_progress: 0,
+                                priority: 2,
+                                created_at: new Date()
+                            })
+                        }, index * 300);
                     }
                 }
             }
-            channel.ack(msg)
-        } else {
-            channel.ack(msg)
-            await eventModel.update({
-                profile_id: profileId,
-                status: "running"
-            }, {
-                $set: {
-                    status: "completed"
-                },
-            })
-            console.log(
-                ' [✔] get item sync stock is complete, no items will be synced',
-                id
-            )
         }
+        channel.ack(msg)
     } catch (error) {
         console.error(' [x] Error: %s', error.message)
         helper.errorLog(id, error.message, {
