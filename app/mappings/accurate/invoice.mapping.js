71353e6db4b7631813c0713b5306b327de32d69a
Rio Riantana <rio.riantana@forstok.com>
2023-09-19 15:29:53 +0700

    handle budle item AV-84

--- app/mappings/accurate/invoice.mapping.js
+++ app/mappings/accurate/invoice.mapping.js
@@ -10,37 +10,61 @@ const invoiceMapping = (order) => {
     const itemQty = {}
 
     for (const item of order.item_lines) {
-        if (itemQty[item.sku]) {
-            detailItems = detailItems.map((obj) => {
-                if (obj.itemNo == item.sku) {
-                    obj.quantity++
-                    // obj.itemCashDiscount +=
-                    //     (item.discount_amount || 0) + item.voucher_amount
+        if (item.bundle_info.length > 0) {
+            item.bundle_info.forEach(item_bundle => {
+                var total_bundle = item.bundle_info.reduce((n, {subtotal}) => n + subtotal, 0)
+                var percent = Number(item.total_price)/Number(total_bundle)
+                let detailItem = {
+                    itemNo: item_bundle.sku, // required; item_lines.id
+                    unitPrice: Math.round(item_bundle.item_price * percent), // || item.total_price || item.price, // required; item_lines.total_price
+                    detailName: (`${item_bundle.name}`).substring(0, 230), // item_lines.variant_name
+                    detailNotes: '', //item_lines.note
+                    // itemCashDiscount:
+                    //     (item.discount_amount || 0) + item.voucher_amount || 0, // item_lines.voucher_amount
+                    quantity: item_bundle.qty,
+                    useTax1: order.taxable
                 }
-                return obj
-            })
-            continue
-        }
-        itemQty[item.sku] = 1
-
-        const detailItem = {
-            itemNo: item.sku, // required; item_lines.id
-            unitPrice: item.sale_price || item.total_price || item.price, // required; item_lines.total_price
-            detailName: (`${item.name} ${item.variant_name || ''}`).substring(0, 230), // item_lines.variant_name
-            detailNotes: item.note || '', //item_lines.note
-            // itemCashDiscount:
-            //     (item.discount_amount || 0) + item.voucher_amount || 0, // item_lines.voucher_amount
-            quantity: 1,
-            useTax1: order.taxable,
-        }
-        if (order.warehouseName) detailItem.warehouseName = order.warehouseName
-
-        if (!order.new_rule) detailItem.salesOrderNumber = order.id
+                if (order.warehouseName) detailItem.warehouseName = order.warehouseName
         
-        if (item.name !== 'Rebate' && item.id !== 'rebate' && item.sku !== 'rebate') {
-            detailItems.push(detailItem)
+                if (!order.new_rule) detailItem.salesOrderNumber = order.id
+                
+                // if (item.name !== 'Rebate' && item.id !== 'rebate' && item.sku !== 'rebate') {
+                    detailItems.push(detailItem)
+                // }
+            
+            });
+        } else {
+            if (itemQty[item.sku]) {
+                detailItems = detailItems.map((obj) => {
+                    if (obj.itemNo == item.sku) {
+                        obj.quantity++
+                        // obj.itemCashDiscount +=
+                        //     (item.discount_amount || 0) + item.voucher_amount
+                    }
+                    return obj
+                })
+                continue
+            }
+            itemQty[item.sku] = 1
+    
+            const detailItem = {
+                itemNo: item.sku, // required; item_lines.id
+                unitPrice: item.sale_price || item.total_price || item.price, // required; item_lines.total_price
+                detailName: (`${item.name} ${item.variant_name || ''}`).substring(0, 230), // item_lines.variant_name
+                detailNotes: item.note || '', //item_lines.note
+                // itemCashDiscount:
+                //     (item.discount_amount || 0) + item.voucher_amount || 0, // item_lines.voucher_amount
+                quantity: 1,
+                useTax1: order.taxable,
+            }
+            if (order.warehouseName) detailItem.warehouseName = order.warehouseName
+    
+            if (!order.new_rule) detailItem.salesOrderNumber = order.id
+            
+            if (item.name !== 'Rebate' && item.id !== 'rebate' && item.sku !== 'rebate') {
+                detailItems.push(detailItem)
+            }
         }
-
     }
 
     const mapped = {
