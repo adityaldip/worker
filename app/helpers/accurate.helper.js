20548fdb808a1c94cdb8613b347299a76792d855
aditya aldi <aldi.prasetya@forstok.com>
2023-10-23 10:25:29 +0700

    summary export event payout on top

--- app/helpers/accurate.helper.js
+++ app/helpers/accurate.helper.js
@@ -317,6 +317,7 @@ class AccurateHelper {
                         }
                     )
                 }else{
+                    await this.sendSummaryExportEvent(ordr,'payment recieve',response.d[0].replace(/"/g, ''),'failed')
                     await this.credentialHandle(message, order)
                     if (order.attempts < maxAttempts) {
                         await delayed.insert({
@@ -335,7 +336,6 @@ class AccurateHelper {
                             $set: { last_error: response, synced: false },
                         }
                     )
-                    await this.sendSummaryExportEvent(ordr,'payment recieve',response.d[0].replace(/"/g, ''),'failed')
                 }
                 throw new Error(message)
             }
