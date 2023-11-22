4ee0ff9e966a17e88604bc689980c4ce0e651d0e
Rio Riantana <rio.riantana@forstok.com>
2023-11-22 15:36:24 +0700

    fix linter

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const StoreItemBulk = require('./app/services/items/create/store.bulk.service')
 
 require('dotenv').config()
 
-var apm = require('elastic-apm-node').start({
+require('elastic-apm-node').start({
     serviceName: 'accurate-worker',
   
     secretToken: 'VRIRaPt5g4TD8jfwAj',
@@ -26,7 +26,7 @@ var apm = require('elastic-apm-node').start({
     serverUrl: 'https://2449190ec8db4b19bfd745112fd94a28.apm.southeastasia.azure.elastic-cloud.com:443',
   
     environment: 'azure-polling'
-  })
+})
 
 async function receiveMessage(channel, queue) {
     channel.assertQueue(queue, {
